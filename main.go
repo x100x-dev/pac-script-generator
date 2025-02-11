e61146b5cb224e8646e3141c777340d7a16797a4
x100x-dev <76058050+x100x-dev@users.noreply.github.com>
2025-02-11 18:43:32 +0300

    Update main.go

--- main.go
+++ main.go
@@ -347,8 +347,8 @@ func main() {
 		"levana.finance": true,		// blocked in RU
 		"aistudio.google.com": true,	// blocked in RU
 		"ai.google.dev": true,		// blocked in RU
-		"alkalimakersuite-pa.clients6.google.com": true,		// domain checks GEO for site aistudio.google.com
-		"waa-pa.clients6.google.com": true,		// domain checks GEO for site aistudio.google.com
+		//"alkalimakersuite-pa.clients6.google.com": true,		// domain checks GEO for site aistudio.google.com
+		//"waa-pa.clients6.google.com": true,		// domain checks GEO for site aistudio.google.com
 		"horizen.io": true,		// blocked in RU
 		"autogeriko.com": true,		// blocked in RU
 		"w.wormhole.com": true,		// blocked in RU
