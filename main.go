eaf80c1cbeb0dd0687eb3d58b2f8e5d82cb89df2
x100x-dev <76058050+x100x-dev@users.noreply.github.com>
2025-03-28 20:35:16 +0300

    Update main.go

--- main.go
+++ main.go
@@ -370,8 +370,11 @@ func main() {
 		"astroport.fi": true,		// blocked in RU
 		"assets.teinon.net": true,	// blocked in RU
 		"pixwox.com": true,		// blocked in RU
+		"pixnoy.com": true,		// blocked in RU
 		"mantra.zone": true,		// blocked in RU
 		"mynearwallet.com": true,	// blocked in RU
+		"btdig.com": true,		// blocked in RU
+		"readymag.com": true,		// blocked in RU
 		
 		
 		// ECH (CloudFlare) ТСПУ
