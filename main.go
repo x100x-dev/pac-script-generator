71ad22bcf9fa3eaf56e5099525d701dc97d894a0
x100x-dev <76058050+x100x-dev@users.noreply.github.com>
2025-05-02 18:06:12 +0400

    Update main.go

--- main.go
+++ main.go
@@ -27,8 +27,8 @@ import (
 	"golang.org/x/text/transform"
 )
 
-var ifForced = flag.Bool("force", false, "If to ignore checking of an updated dump.csv available")
-//var ifForced = flag.Bool("force", true, "If to ignore checking of an updated dump.csv available")
+//var ifForced = flag.Bool("force", false, "If to ignore checking of an updated dump.csv available")
+var ifForced = flag.Bool("force", true, "If to ignore checking of an updated dump.csv available")
 
 type blockProvider struct {
 	urls   []string
@@ -377,6 +377,7 @@ func main() {
 		"readymag.com": true,		// blocked in RU
 		"mintchain.io": true,		// blocked in RU
 		"sndcdn.com": true,		// ТСПУ
+		"arras.io": true,		// ТСПУ
 		
 		
 		// ECH (CloudFlare) ТСПУ
