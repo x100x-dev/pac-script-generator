c6ddf530077ced9b6134c55fb3b64a629421bdfb
x100x-dev <76058050+x100x-dev@users.noreply.github.com>
2025-06-05 00:27:45 +0400

    Update main.go

--- main.go
+++ main.go
@@ -36,24 +36,24 @@ type blockProvider struct {
 }
 
 var blockProviders = []blockProvider{
-	blockProvider{
-		urls: []string{
-			"https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv",
-		},
-		rssUrl: "https://github.com/zapret-info/z-i/commits/master.atom",
-	},	
+	//blockProvider{
+	//	urls: []string{
+	//		"https://raw.githubusercontent.com/zapret-info/z-i/master/dump.csv",
+	//	},
+	//	rssUrl: "https://github.com/zapret-info/z-i/commits/master.atom",
+	//},	
 	//blockProvider{
 	//	urls: []string{
 	//		"https://svn.code.sf.net/p/zapret-info/code/dump.csv",
 	//	},
 	//	rssUrl: "https://sourceforge.net/p/zapret-info/code/feed",
 	//},
-	//blockProvider {
-	//	urls: []string{
-	//		"https://app.assembla.com/spaces/z-i/git/source/master/dump.csv?_format=raw",
-	//	},
-	//	rssUrl: "https://app.assembla.com/spaces/z-i/stream.rss",
-	//},
+	blockProvider {
+		urls: []string{
+			"https://app.assembla.com/spaces/z-i/git/source/master/dump.csv?_format=raw",
+		},
+		rssUrl: "https://app.assembla.com/spaces/z-i/stream.rss",
+	},
 }
 
 var get = func(url string) (*http.Response, error) {
