a4a6a689a6879f1c6a2fb461dbbdc5ca85bd7c1e
x100x-dev <76058050+x100x-dev@users.noreply.github.com>
2026-01-16 15:33:11 +0400

    Update main.go

--- main.go
+++ main.go
@@ -592,6 +592,7 @@ func main() {
 		"daumcdn.net": true,		// Amazon
 		"forklog.com": true,		// DigitalOcean
 		"anwap-films.com": true,		// ТСПУ
+		"1luxtv.net": true,		// ТСПУ
 		
 		
 		// ECH (CloudFlare) ТСПУ
