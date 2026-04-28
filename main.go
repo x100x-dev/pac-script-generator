891f787f7a0f9e2b4c6099c4c7154292796fbd5b
x100x-dev <76058050+x100x-dev@users.noreply.github.com>
2026-04-28 11:31:09 +0400

    Update main.go

--- main.go
+++ main.go
@@ -45,7 +45,7 @@ var blockProviders = []blockProvider{
 	blockProvider{
 		urls: []string{
 			//"https://svn.code.sf.net/p/zapret-info/code/dump.csv",
-			"https://raw.githubusercontent.com/bol-van/rulist/refs/heads/main/reestr_hostname.txt",
+			"https://antifilter.download/list/domains.lst",
 		},
 		rssUrl: "https://sourceforge.net/p/zapret-info/code/feed",
 	},
